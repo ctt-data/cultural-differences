2079623df211a195719e9ef3fa9d573aa28765ee
Emelkayabetz <45092342+Emelkayabetz@users.noreply.github.com>
2018-11-16 10:58:49 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,6 +2,24 @@
 $( document ).ready(function() {
 
 	// Start editing here
+$("#first").click(function(){
+username = $("#user_input").val()
+message = "Great, let´s be friends!"
+   $("#answer_text").text(message)
+  })
+
+$("#second").click(function(){
+username = $("#user_input").val()
+message = "Too bad!"
+   $("#answer_text").text(message)
+  })
+
+$("#third").click(function(){
+username = $("#user_input").val()
+message = "Try again!"
+   $("#answer_text").text(message)
+  })
+
 
 
 	// Stop editing here
