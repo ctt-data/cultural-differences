76bbac0a69bc73652be58e37eb838b61d0c025b9
diananicole <45092363+diananicole@users.noreply.github.com>
2018-11-16 11:45:33 +0100

    Update javascript.js
    From my CodePen

--- javascript.js
+++ javascript.js
@@ -2,23 +2,49 @@
 $( document ).ready(function() {
 
 	// Start editing here
-$("#first").click(function(){
-username = $("#user_input").val()
-message = "Great, let´s be friends!"
-   $("#answer_text").text(message)
-  })
+	
+$("#answer_question_1").hide();
+$("#question_2").hide();
 
-$("#second").click(function(){
-username = $("#user_input").val()
-message = "Too bad!"
-   $("#answer_text").text(message)
-  })
+$("#first").click(function() {
+  message = "Correct!";
+  $("#answer_text").text(message);
+  $("#answer_question_1").show();
+});
 
-$("#third").click(function(){
-username = $("#user_input").val()
-message = "Try again!"
-   $("#answer_text").text(message)
-  })
+$("#second").click(function() {
+  message = "Try again!";
+  $("#answer_question_1").hide();
+  $("#answer_text").text(message);
+});
+
+$("#third").click(function() {
+  message = "Not quite!";
+  $("#answer_question_1").hide();
+  $("#answer_text").text(message);
+});
+
+$("#next_question").click(function() {
+  $("#question_2").show();
+  $("#answer_text").text(message);
+});
+
+$("#fourth").click(function() {
+  message = "Correct!";
+  $("#answer_text_2").text(message);
+  //show function
+});
+
+$("#fifth").click(function() {
+  message = "Try again!";
+  $("#answer_text_2").text(message);
+});
+
+$("#sixth").click(function() {
+  username = $("#user_input").val();
+  message = "Not quite!";
+  $("#answer_text_2").text(message);
+});
 
 
 
